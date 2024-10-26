63bb14ea275873e390b4e986ba49edb1f706ffba
ViniciusLopes <vinicius514563@gmail.com>
2024-10-25 23:23:27 -0300

    documentações

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ import sympy as sp
 import pandas as pd
 from IPython.display import display
 
+
 class MetodoBissecao:
-    def __init__(self, A, B, precisao, funcao):
-        self.a = A
-        self.b = B
+
+    def __init__(self, a, b, precisao, funcao):
+        """Atributos necessarios para inicializar a classe"""
+        self.a = a
+        self.b = b
         self.precisao_usuario = precisao
         self.funcao = funcao
 
-    def calcular_funcao(self, funcao, x_valor):
+    def _calcular_funcao(self, funcao, x_valor):
+        """Recebe a string de uma função e transforma na função
+        É usado apenas no método final"""
         x = sp.Symbol('x')
         funcao = sp.sympify(funcao)
-        return funcao.subs(x, x_valor)
+        resultado = funcao.subs(x, x_valor)
+        return round(float(resultado), 5)
 
     def _calcular_Xi(self, a, b):
-        xi = (a + b) / 2
-        return xi
+        """Calcula Xi"""
+        try:
+            xi = (a + b) / 2
+            return xi
+        except:
+            print("Erro ao calcular xi")
 
     def _calcular_precisao_final(self, a, b):
+        """"Calula precisao final"""
         precisao_final = b - a
         return precisao_final
 
@@ -31,9 +42,9 @@ class MetodoBissecao:
         while True:
             contador += 1
             xi = self._calcular_Xi(a, b)
-            f_a = self.calcular_funcao(self.funcao, a)
-            f_b = self.calcular_funcao(self.funcao, b)
-            f_xi = self.calcular_funcao(self.funcao, xi)
+            f_a = self._calcular_funcao(self.funcao, a)
+            f_b = self._calcular_funcao(self.funcao, b)
+            f_xi = self._calcular_funcao(self.funcao, xi)
             precisao_final = self._calcular_precisao_final(a, b)
             iteracoes.append([contador, a, b, xi, f_a, f_b, f_xi, precisao_final])
 
@@ -48,22 +59,26 @@ class MetodoBissecao:
             if self.verificar_precisao(precisao_final, self.precisao_usuario):
                 break
 
-        return iteracoes, xi, contador
+        return iteracoes, f_xi, contador
 
     def verificar_precisao(self, precisao_final, precisao_usuario):
         return precisao_final <= precisao_usuario
 
+
 def main():
     A = float(input("Informe o valor de A: "))
     B = float(input("Informe o valor de B: "))
-    precisao = float(input("Informe a precisão desejada: "))
+    precisao = str(input("Informe a precisão desejada: "))
+    precisao = precisao.replace(",", ".")
+    precisao = float(precisao)
     funcao = input("Informe a função f(x): ")
     trabalho = MetodoBissecao(A, B, precisao, funcao)
     iteracoes, valor_final, total_iteracoes = trabalho.metodo_bissecao()
     df = pd.DataFrame(iteracoes, columns=['Iteração', 'A', 'B', 'Xi', 'f(A)', 'f(B)', 'f(Xi)', 'Precisão'])
     display(df)
     print(f"\nPrecisão atingida após {total_iteracoes} iterações.")
-    print(f"O valor final aproximado da raiz é: {valor_final}")
+    print(f"O valor final aproximado da F(x{total_iteracoes}) = {valor_final}")
+
 
 if __name__ == "__main__":
     main()
